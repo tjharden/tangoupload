66393f67954006d241c19476cd726c35e716944d
tjharden <75020661+tjharden@users.noreply.github.com>
2021-05-11 15:42:39 +0100

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,9 +18,9 @@ class Config(object):
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 500000000000
-    TG_MAX_FILE_SIZE = 20971520000000
-    FREE_USER_MAX_FILE_SIZE = 500000000000
+    MAX_FILE_SIZE = 50000000000000
+    TG_MAX_FILE_SIZE = 2097152000000000
+    FREE_USER_MAX_FILE_SIZE = 50000000000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
@@ -33,6 +33,6 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 360000
+    PROCESS_MAX_TIMEOUT = 3600000
     # watermark file
     DEF_WATER_MARK_FILE = ""
