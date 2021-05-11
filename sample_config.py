672959bd6a48c1e7e78420333e908505c3291eb2
tjharden <75020661+tjharden@users.noreply.github.com>
2021-05-11 11:08:11 +0100

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,11 +18,11 @@ class Config(object):
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 5000000000000
-    TG_MAX_FILE_SIZE = 209715200000000
-    FREE_USER_MAX_FILE_SIZE = 5000000000000
+    MAX_FILE_SIZE = 500000000000
+    TG_MAX_FILE_SIZE = 20971520000000
+    FREE_USER_MAX_FILE_SIZE = 500000000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 512))
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # proxy for accessing youtube-dl in GeoRestricted Areas
